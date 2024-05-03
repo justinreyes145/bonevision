3ea93c7010e4fb0f09c734286ce28f1c7494c1d1
Michael Farrell <mpf0014@gmail.com>
2024-05-02 19:40:33 -0700

    Classifier updates

--- BVapp/classifier.py
+++ BVapp/classifier.py
@@ -9,6 +9,7 @@ import time
 from skimage import transform
 from skimage.io import imread
 
+import threading
 
 def load_frac_model():
     with open('models/model.json', 'r') as model_data:
@@ -21,36 +22,49 @@ def load_bone_model():
         loaded_model_json = model_data.read()
         return keras.models.model_from_json(loaded_model_json)
 
+def thread_load_all_models():
+    st = time.time()
+    bone_model = load_bone_model()
+    bone_model.load_weights('models/bone_weights.keras')
+    bone_model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    elbow_model = load_frac_model()
+    elbow = True
+    print(f'Time elapsed = {time.time() - st} sec (Elbow)')
+
+    finger_model = keras.models.load_model('models/dense169_finger_70_07_acc.keras', compile=False)
+    finger = True
+
+    forearm_model = keras.models.load_model('models/dense169_forearm_75_75_acc.keras', compile=False)
+    forearm = True
+
+    hand_model = load_frac_model()
+    hand = True
+
+    humerus_model = load_frac_model()
+    humerus = True
+
+    shoulder_model = keras.models.load_model('models/dense169_shoulder_70_69_acc.keras', compile=False)
+    shoulder = True
+
+    wrist_model = keras.models.load_model('models/dense169_wrist_75_42_acc.keras', compile=False)
+    wrist = True
+
+    print(f'Time elapsed = {time.time() - st} sec (Bone)')
+    st = time.time()
+    
 
 # Load all model architectures
-st = time.time()
-elbow_model = load_frac_model()
-elbow = True
-print(f'Time elapsed = {time.time() - st} sec (Elbow)')
-
-finger_model = keras.models.load_model('models/dense169_finger_70_07_acc.keras', compile=False)
-finger = True
-
-forearm_model = keras.models.load_model('models/dense169_forearm_75_75_acc.keras', compile=False)
-forearm = True
-
-hand_model = load_frac_model()
-hand = True
-
-humerus_model = load_frac_model()
-humerus = True
-
-shoulder_model = keras.models.load_model('models/dense169_shoulder_70_69_acc.keras', compile=False)
-shoulder = True
-
-wrist_model = keras.models.load_model('models/dense169_wrist_75_42_acc.keras', compile=False)
-wrist = True
-
-st = time.time()
-bone_model = load_bone_model()
-bone_model.load_weights('models/bone_weights.keras')
-bone_model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-print(f'Time elapsed = {time.time() - st} sec (Bone)')
+bone_model = None
+elbow_model = None
+finger_model = None
+forearm_model = None
+hand_model = None
+humerus_model = None
+shoulder_model = None
+wrist_model = None
+model_loading_thread = threading.Thread(target=thread_load_all_models)
+model_loading_thread.start()
 
 bone_labels = ['ELBOW', 'FINGER', 'FOREARM', 'HAND', 'HUMERUS', 'SHOULDER', 'WRIST']
 
@@ -73,6 +87,9 @@ def load_image(image_path, size):
 def predict_elbow(image_path):
     global elbow
     if elbow:
+        # Wait for model load
+        while elbow_model == None:
+            continue
         st = time.time()
         elbow_model.load_weights('models/elbow_86.67_weights.keras')
         elbow_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
@@ -85,6 +102,9 @@ def predict_elbow(image_path):
 def predict_finger(image_path):
     global finger
     if finger:
+        # Wait for model load
+        while finger_model == None:
+            continue
         st = time.time()
         finger_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
         print(f'Time elapsed = {time.time() - st} sec (Finger)')
@@ -94,8 +114,12 @@ def predict_finger(image_path):
 
 
 def predict_forearm(image_path):
+
     global forearm
     if forearm:
+        # Wait for model load
+        while forearm_model == None:
+            continue
         st = time.time()
         forearm_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
         print(f'Time elapsed = {time.time() - st} sec (Forearm)')
@@ -107,6 +131,9 @@ def predict_forearm(image_path):
 def predict_hand(image_path):
     global hand
     if hand:
+        # Wait for model load
+        while hand_model == None:
+            continue
         st = time.time()
         hand_model.load_weights('models/hand_79.96_weights.keras')
         hand_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
@@ -119,6 +146,9 @@ def predict_hand(image_path):
 def predict_humerus(image_path):
     global humerus
     if humerus:
+        # Wait for model load
+        while humerus_model == None:
+            continue
         st = time.time()
         humerus_model.load_weights('models/humerus_88.89_weights.keras')
         humerus_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
@@ -131,6 +161,9 @@ def predict_humerus(image_path):
 def predict_shoulder(image_path):
     global shoulder
     if shoulder:
+        # Wait for model load
+        while shoulder_model == None:
+            continue
         st = time.time()
         shoulder_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
         print(f'Time elapsed = {time.time() - st} sec (Shoulder)')
@@ -142,6 +175,9 @@ def predict_shoulder(image_path):
 def predict_wrist(image_path):
     global wrist
     if wrist:
+        # Wait for model load
+        while wrist_model == None:
+            continue
         st = time.time()
         wrist_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
         print(f'Time elapsed = {time.time() - st} sec (Wrist)')
